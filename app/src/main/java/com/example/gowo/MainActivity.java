6faac1f717cf3b4212111bb00b70444e49de50b4
labianchini <larissa.bianchinim@gmail.com>
2020-12-05 23:39:57 -0300

    começo do desenvolvimento da página home

--- app/src/main/java/com/example/gowo/MainActivity.java
+++ app/src/main/java/com/example/gowo/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.gowo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +19,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //oi
+        ImageButton imgBtnFacebook = findViewById(R.id.imgBtnFacebook);  // coloquei esse código só para eu conseguir mudar de página
+        imgBtnFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
