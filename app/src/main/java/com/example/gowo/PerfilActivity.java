8956aa93dd00153c47f3653f43fba9e9777f8ee1
LarissaCamposVieira <larissacamposvp@gmail.com>
2021-02-05 13:28:45 -0300

    Merge branch 'master' of C:\Users\gabri\AndroidStudioProjects\GoWo with conflicts.

--- app/src/main/java/com/example/gowo/PerfilActivity.java
+++ app/src/main/java/com/example/gowo/PerfilActivity.java
@@ -39,24 +39,8 @@ public class PerfilActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        
 
-        Button btnAjuda = findViewById(R.id.btnAjuda);
-        btnAjuda.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(PerfilActivity.this, AjudaActivity.class);
-                startActivity(i);
-            }
-        });
-
-        Button btnConfig = findViewById(R.id.btnConfig);
-        btnConfig.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(PerfilActivity.this, ConfiguracoesActivity.class);
-                startActivity(i);
-            }
-        });
     }
 
     @Override
