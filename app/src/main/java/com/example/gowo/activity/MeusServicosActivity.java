e9b06f786a1f28a051115aa32353cc8212b543b3
LarissaCamposVieira <larissacamposvp@gmail.com>
2021-04-04 13:52:56 -0300

    Botao adicionar serviço

--- app/src/main/java/com/example/gowo/activity/MeusServicosActivity.java
+++ app/src/main/java/com/example/gowo/activity/MeusServicosActivity.java
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -38,7 +39,11 @@ public class MeusServicosActivity extends AppCompatActivity {
         BtnAddServ.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MeusServicosActivity.this, CadastrarServicoActivity.class);
+                //Intent i = new Intent(MeusServicosActivity.this, CadastrarServicoActivity.class);
+                //startActivity(i);
+                String url = "http://gowoifes.herokuapp.com/views/home/work/";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
                 startActivity(i);
             }
         });
